1f88e383e886a170da5515bb92a9f34251b3f7e5
Bolot-u <bolot.moldoev.ur@gmail.com>
2023-02-12 23:57:03 +0100

    'hw2'

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ TOKEN = config('TOKEN')
 bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
+@db.message_handler(commands=['hello'])
+async def start_handler(message: types.Message):
+            await bot.send_message(message.from_user.id, f'hello {message.from_user.first_name}')
+            await message.answer('пока что все')
+
+@db.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
+
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(db, skip_updates=True)
